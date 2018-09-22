6418649347bc6e9fbd0931e7af09a7037609ee50
asselyer <asel.yer98@gmail.com>
2018-09-22 13:47:15 +0600

    ~

---
+++ week1/example2.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+
+#while a <= b:
+
+#for x in range(a, b):
+#while a <= b:
+for x in range(a, b+1):
+    if x%2==0:
+
+
+        print(x, end=" ")
+        x += 1
+      #  x += 1
+
+    #break
