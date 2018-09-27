7cd3a510bdabbd4a8f1d54309e92560922d23d01
asselyer <42924678+asselyer@users.noreply.github.com>
2018-09-27 08:26:53 +0600

    Add files via upload

---
+++ week1/example4.py
@@ -0,0 +1,5 @@
+a = int(input())
+c = input()
+for i in range(a):
+    b = c.split()
+print(b)
---
+++ week1/example5.py
@@ -0,0 +1,6 @@
+a = int(input())
+c = input()
+for i in range(a):
+    b = c.split()
+    if i%2==0:
+        print(b[i], end=" ")
